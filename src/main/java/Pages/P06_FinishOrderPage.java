8bb477a9fa5230b09a2ca614eae4d4440a8f18c9
ahmedhelmiQC <ahmed.systemtester@gmail.com>
2024-10-03 16:36:58 +0300

    TC04_Checkout.java

---
+++ src/main/java/Pages/P06_FinishOrderPage.java
@@ -0,0 +1,25 @@
+package Pages;
+
+import Utilities.Utility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static Utilities.Utility.findWebElement;
+
+public class P06_FinishOrder {
+
+    private final By thanksMessage = By.tagName("h2");
+    private final By homePage = By.id("back-to-products");
+    private final WebDriver driver;
+    public P06_FinishOrder(WebDriver driver) {
+        this.driver = driver;
+    }
+    public boolean checkVisibilityOfThanksMessage(){
+        return findWebElement(driver,thanksMessage).isDisplayed();
+    }
+
+   public P02_LandingPage goToHmePage(){
+        Utility.clickingOnElement(driver,homePage);
+        return new P02_LandingPage(driver);
+   }
+}
--- src/test/java/Tests/T06_FinishOrderTest.java
+++ src/test/java/Tests/T06_FinishOrderTest.java
@@ -7,8 +7,10 @@ import Utilities.Data_Utilis;
 import Utilities.LogsUtilis;
 import Utilities.Utility;
 import com.github.javafaker.Faker;
-import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -16,15 +18,14 @@ import java.time.Duration;
 
 import static DriverFactory.DriverFactory.*;
 import static Utilities.Data_Utilis.getPropertyValue;
-
-@Listeners({IInvokedMethodListenerClass.class, ITestResultListenerClass.class})
-public class TC04_Checkout {
+@Listeners ({IInvokedMethodListenerClass.class, ITestResultListenerClass.class})
+public class T06_FinishOrder {
     private final String UserName= Data_Utilis.getJsonData("validLogin","username");
     private final String Password= Data_Utilis.getJsonData("validLogin","password");
     private final String FirstName = Data_Utilis.getJsonData("information","fName")+"-"+ Utility.getTimestamp();
     private final String LastName = Data_Utilis.getJsonData("information","lName")+"-"+Utility.getTimestamp();
     private final String ZipCode = new Faker().number().digits(5);
-    public TC04_Checkout() throws FileNotFoundException {
+    public T06_FinishOrder() throws FileNotFoundException {
     }
 
     @BeforeMethod
@@ -39,11 +40,14 @@ public class TC04_Checkout {
     public void checkoutStepOneTC () throws IOException {
         new P01_LoginPage(getDriver()).enterUserName(UserName)
                 .enterPassword(Password).clickOnLoginButton()
-                .addRandomProducts(3,6)
+                .addAllProductsToCart()
                 .clickOnCartIcon().clickOnCheckOnButton()
-                .fillingInformationForm(FirstName,LastName,ZipCode).clickOnContinueButton();
+                .fillingInformationForm(FirstName,LastName,ZipCode).clickOnContinueButton().clickingFinishButton().goToHmePage();
         LogsUtilis.info(FirstName + " "+ LastName + " " + ZipCode);
-        Assert.assertTrue(Utility.VerifyURL(getDriver(),getPropertyValue("environment","Checkout_URL")));
+//        String message = new P06_FinishOrder(getDriver()).getCompleteMessage();
+//        LogsUtilis.info("Thanks Message : "+ message);
+//        System.out.println(message);
+////        Assert.assertTrue(new P05_OverviewPage(getDriver()).comparingPrices());
     }
     @AfterMethod
     public void quite() throws IOException {
