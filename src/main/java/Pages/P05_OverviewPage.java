b390147bbf582cec1654b8e793116ccc4ee6caa2
ahmedhelmiQC <ahmed.systemtester@gmail.com>
2024-10-03 16:38:51 +0300

    TC05_verviewPage.java - src/main/java/Pages/P05_OverviewPage.java

--- src/main/java/Pages/P05_OverviewPage.java
+++ src/main/java/Pages/P05_OverviewPage.java
@@ -1,12 +1,43 @@
 package Pages;
 
+import Utilities.LogsUtilis;
+import Utilities.Utility;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class P05_OverviewPage {
 
+    private final By subTotal = By.className("summary_subtotal_label");
+    private final By tax = By.className("summary_tax_label");
+    private final By total = By.className("summary_total_label");
+    private final By buttonFinish = By.id("finish");
     private final WebDriver driver;
 
     public P05_OverviewPage(WebDriver driver) {
         this.driver = driver;
     }
+    public float getSubtotal(){
+        LogsUtilis.info("Sub Total Price :"+Utility.getText(driver,subTotal).replace("Item total: $",""));
+        return  Float.parseFloat(Utility.getText(driver,subTotal).replace("Item total: $",""));
+    }
+    public float getTax(){
+        LogsUtilis.info("Tax :" + Utility.getText(driver,tax).replace("Tax: $",""));
+        return Float.parseFloat(Utility.getText(driver,tax).replace("Tax: $",""));
+    }
+    public float getTotal(){
+        LogsUtilis.info("Actual Total Price :" + Utility.getText(driver,total).replace("Total: $","") );
+        return Float.parseFloat(Utility.getText(driver,total).replace("Total: $",""));
+    }
+    public String calculateTotalPrice(){
+        LogsUtilis.info("Total Price : "+ getSubtotal()+getTax());
+        return String.valueOf(getSubtotal()+getTax());
+    }
+    public boolean comparingPrices(){
+        return calculateTotalPrice().equals(String.valueOf(getTotal()));
+    }
+    public P06_FinishOrderPage clickingFinishButton(){
+        Utility.clickingOnElement(driver,buttonFinish);
+        return new P06_FinishOrderPage(driver);
+    }
 }
+
--- src/test/java/Tests/TC05_Overview.java
+++ src/test/java/Tests/TC05_Overview.java
@@ -1,4 +1,56 @@
 package Tests;
 
+import Listeners.IInvokedMethodListenerClass;
+import Listeners.ITestResultListenerClass;
+import Pages.P01_LoginPage;
+import Pages.P05_OverviewPage;
+import Utilities.Data_Utilis;
+import Utilities.LogsUtilis;
+import Utilities.Utility;
+import com.github.javafaker.Faker;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+
+import static DriverFactory.DriverFactory.*;
+import static Utilities.Data_Utilis.getPropertyValue;
+@Listeners ({IInvokedMethodListenerClass.class, ITestResultListenerClass.class})
 public class TC05_Overview {
+    private final String UserName= Data_Utilis.getJsonData("validLogin","username");
+    private final String Password= Data_Utilis.getJsonData("validLogin","password");
+    private final String FirstName = Data_Utilis.getJsonData("information","fName")+"-"+ Utility.getTimestamp();
+    private final String LastName = Data_Utilis.getJsonData("information","lName")+"-"+Utility.getTimestamp();
+    private final String ZipCode = new Faker().number().digits(5);
+    public TC05_Overview() throws FileNotFoundException {
+    }
+
+    @BeforeMethod
+    public void setup() throws IOException {
+        setupDriver(getPropertyValue("environment","Browser"));
+        LogsUtilis.info("EdgeDriver is opened");
+        getDriver().get(getPropertyValue("environment","BASE_URL"));
+        LogsUtilis.info("Page is redirect to the Home Page ");
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @Test
+    public void checkoutStepOneTC () throws IOException {
+        new P01_LoginPage(getDriver()).enterUserName(UserName)
+                .enterPassword(Password).clickOnLoginButton()
+                .addAllProductsToCart()
+                .clickOnCartIcon().clickOnCheckOnButton()
+                .fillingInformationForm(FirstName,LastName,ZipCode).clickOnContinueButton();
+        LogsUtilis.info(FirstName + " "+ LastName + " " + ZipCode);
+
+        Assert.assertTrue(new P05_OverviewPage(getDriver()).comparingPrices());
+    }
+    @AfterMethod
+    public void quite() throws IOException {
+        quiteDriver();
+    }
 }
