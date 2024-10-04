0561722b5ad33fcc435ebde375cd73e15adfc33e
ahmedhelmiQC <ahmed.systemtester@gmail.com>
2024-10-04 23:42:41 +0300

    Test Runner

--- src/main/java/Pages/P06_FinishOrderPage.java
+++ src/main/java/Pages/P06_FinishOrderPage.java
@@ -3,6 +3,8 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import static Utilities.Utility.findWebElement;
+
 public class P06_FinishPage {
     private final WebDriver driver;
 
@@ -10,6 +12,10 @@ public class P06_FinishPage {
     private final By backToHomePage = By.id("back-to-products");
     public P06_FinishPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public boolean checkVisibilityOfThanksMessage(){
+        return findWebElement(driver,thanksMessage).isDisplayed();
     }
 
 
